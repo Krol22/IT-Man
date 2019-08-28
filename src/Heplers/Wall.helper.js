32631ed30aa3a63c084c5bfd585b0868619effa5
Karol Toruń <Karol@brainhub.pl>
2019-08-28 11:34:51 +0200

    Finished wall generator

--- src/Heplers/Wall.helper.js
+++ src/Heplers/Wall.helper.js
@@ -13,34 +13,26 @@ function generateWall (mapGenerator, x, y) {
       }
 
       if (map[x + i - 1][ x + j - 1] === 6) {
-        walls.push({ x: i, y: j });
+        walls.push({ x: j, y: i });
       }
     }
   } 
 
   switch(walls.length) {
     case 0: {
-      return {
-        type: 0,
-      };
+      return { type: 0 };
     }
     case 1: {
-      const wall = {
-        type: 1,
-      };
-
+      const wall = { type: 1, };
       const {x, y} = walls[0];
-      if (x === 0) {
-        wall.rotation = 0; 
-      } 
-      if (x === 2) {
-        wall.rotation = 2; 
-      }
-      if (y === 0) {
-        wall.rotation = 3; 
-      }
-      if (y === 2) {
-        wall.rotation = 1; 
+      if (x === 0) { 
+        wall.rotation = 0;
+      } else if (y === 2) { 
+        wall.rotation = 1;
+      } else if (x === 2) { 
+        wall.rotation = 2;
+      } else if (y === 0) { 
+        wall.rotation = 3;
       }
       return wall;
     }
@@ -48,18 +40,33 @@ function generateWall (mapGenerator, x, y) {
       const [wall1, wall2] = walls;
       if (Math.abs(wall1.x - wall2.x) === 2 || Math.abs(wall1.y - wall2.y) === 2)  {
         return {
-          type: 2
+          type: 2,
+          rotation: wall1.x === wall2.x ? 0 : 1,
         };
       }
 
-      return {
-        type: 3
-      };
+      const wall = { type: 3 };
+      if (wall2.x === 2) {
+        wall.rotation = 0;
+      } else if (wall1.x === 2) {
+        wall.rotation = 1;
+      } else if (wall1.x === 0) {
+        wall.rotation = 2;
+      } else if (wall2.x === 0) {
+        wall.rotation = 3;
+      }
+      return wall;
     }
     case 3: {
-      return {
-        type: 4
-      };
+      const wall = { type: 4 };
+      const [wall1, wall2, wall3] = walls;
+      if (wall2.y === wall3.y || wall1.y === wall2.y) { // 1, 3
+        wall.rotation = wall1.x === 1 ? 3 : 1; 
+        return wall;
+      }
+      // 0, 2
+      wall.rotation = wall2.x === 0 ? 2 : 0;
+      return wall;
     }
     case 4: {
       return {
